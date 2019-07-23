537b465d16d61b6c8ed0e9e89b98ccf4800196de
DarkIlyas <43656060+DarkIlyas@users.noreply.github.com>
2019-07-23 12:08:36 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+
+const firebaseConfig = {
+   // 1 - Access to the firebase console
+   // 2 - Choose the project where you want to have the db imported/exported
+   // 3- Project Overview / click on Project settings
+   // 4- Look down for Firebase SDK snippet , choose configuration and copy it here 
+  };
+  // Exports
+  module.exports = firebaseConfig;
